5b256cf34d861175ade9b58fdd3df16705c1c89f
Rajesh Vijayan <rajmvjn@gmail.com>
2022-05-24 11:16:32 +0530

    prod build setup added for prod n dev not qa

--- src/constants/apiEndPoints.ts
+++ src/constants/apiEndPoints.ts
@@ -1,12 +1,15 @@
+let base_url = "http://localhost:8080/";
+if (process.env.NODE_ENV === "production") {
+  base_url = "https://profile-app-mw.herokuapp.com/";
+}
+
 const API_ENDPOINTS = {
-  // comment
   POST_COMMENT: "/user/comment",
   USER_AUTH: "/user/login",
   GITHUB_PROFILE: "https://github.com/rajmvjn",
   LINKEDIN_PROFILE: "https://www.linkedin.com/in/rajesh-vijayan-294841190/",
   TWITTER_PROFILE: "https://twitter.com/rajmvjn",
-  // API_BASE_URL: "https://profile-app-mw.herokuapp.com/",
-  API_BASE_URL: "http://localhost:8080/",
+  API_BASE_URL: base_url,
 };
 
 export default API_ENDPOINTS;
